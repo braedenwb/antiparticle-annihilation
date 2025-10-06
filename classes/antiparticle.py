c688098666234f3e5c102705302130f97ea015d0
Braeden <156366121+braedenwb@users.noreply.github.com>
2025-10-06 07:39:11 -0500

    Delete classes directory

--- classes/antiparticle.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-class Antiparticle(pygame.sprite.Sprite):
-    def __init__(self, pos, image):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = image
-        self.rect = self.image.get_rect()
-        self.rect.center = pos
--- classes/element.py
+++
@@ -1,6 +0,0 @@
-class Element():
-    def __init__(self, image, damage, health, speed):
-        self.image = image
-        self.damage = damage
-        self.health = health
-        self.speed = speed
